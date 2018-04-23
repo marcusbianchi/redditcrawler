4fe721eaa466441a4b6e0b56d0146e44cf82d61a
Marcus <marcusv.bianchisantos@gmail.com>
2018-04-23 15:32:07 -0300

    Added first interaction check...

--- bot.py
+++ bot.py
@@ -13,17 +13,21 @@ pp = pprint.PrettyPrinter(indent=2)
 def handle(msg):
     chat_id = msg['chat']['id']
     message = msg['text'].split(" ")
-    bot.sendMessage(chat_id, "Aguarde enquanto nosso gerador de Inutilidades procura o Reddit")
     try:
         if(len(message)==2):
+            bot.sendMessage(chat_id, "Aguarde enquanto nosso gerador de Inutilidades procura no Reddit")
             command = message[0]
             args  = message[1]           
             if command == '/NadaPraFazer':
-                process_crawler(args,chat_id)
-            elif command == '/start':
-                bot.sendMessage(chat_id, "Olá eu sou o seu buscador de Inutilidades digite /NadaPraFazer [+ Lista de subrredits separados por ;] para obter lista do que fazer")
+                process_crawler(args,chat_id)            
             else:                
                 bot.sendMessage(chat_id, "Não entendi, digite /NadaPraFazer [+ Lista de subrredits separados por ;] para obter lista do que fazer")
+        if(len(message)==1):
+            if command == '/NadaPraFazer':
+                process_crawler(["news","cats"],chat_id)      
+            command = message[0]
+            elif command == '/start':
+                bot.sendMessage(chat_id, "Olá eu sou o seu buscador de Inutilidades digite /NadaPraFazer [+ Lista de subrredits separados por ;] para obter lista do que fazer")
         else:
             bot.sendMessage(chat_id, "Não entendi, digite /NadaPraFazer [+ Lista de subrredits separados por ;] para obter lista do que fazer")
     except Exception as e:
@@ -37,10 +41,13 @@ def process_crawler(args,chat_id):
     reddits = args.split(';')
     for reddit in reddits: 
         cur_result = main("/r/"+reddit,False)
+        print(reddit)
+        print(cur_result)
         if(len(cur_result)!=0):
             results.extend(cur_result) 
     if(len(results)==0):              
         bot.sendMessage(chat_id,"Infelizmente não houve respostas para este tópico, tente gatinhos(cats)")
+    pp.pprint(results)
     for result in results:
         bot.sendMessage(chat_id,"De uma olhada na Thread: "+ result['title'] + " Link: "+ result['link'] + " No subreddit: https://www.reddit.com" +result['subreddit'] + " Veja os comentários em: https://www.reddit.com/" + result['comments'] + " Ela tem  "+ result['upvotes']+" upvotes")
 key = os.environ['BOOT_KEY_REDDIT']
